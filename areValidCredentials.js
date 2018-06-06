5f37e5a2f80ed640f3f4890ab6cc0b5ecd280c57
carolinaraya <_carolina_@hotmail.cl>
2018-06-05 20:51:37 -0400

    quite ? true : false del op ternario, no era necesario

--- areValidCredentials.js
+++ areValidCredentials.js
@@ -1,4 +1,4 @@
 function areValidCredentials(name, password) {
-    output = name.length > 3 && password.length >= 8 ? true : false;
+    output = name.length > 3 && password.length >= 8;
     return output
 }
